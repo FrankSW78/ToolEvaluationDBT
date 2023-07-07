b098270b74b5fb28783dbca31d7ee154adc38d3b
frank.weidlich@siemens-healthineers.com <frank.weidlich@siemens-healthineers.com>
2023-07-07 08:18:52 +0000

    add column countrycode

--- models/eval_prototype/source_are.sql
+++ models/eval_prototype/source_are.sql
@@ -1,4 +1,4 @@
 {{ config(materialized='table') }}
 
-select ARE, NAME, SHORTNAME,  TOWN, COUNTRY
+select ARE, NAME, SHORTNAME,  TOWN, COUNTRY, COUNTRYCODE
 from {{ source('source_database', 'TEMP_LEGALENTITY_PROTOTYPE') }}
